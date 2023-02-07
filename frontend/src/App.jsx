a9c447f7af59a7df21cce564654b5af2081ae9c7
zetsuen <kevnhalim@gmail.com>
2023-02-07 15:34:37 +0700

    feat : adding navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import { About, Footer, Header, Projects, Skills, Testimonials } from './containers';
+import { Navbar } from './components';
+import './App.scss';
 
 const App = () => {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Projects />
