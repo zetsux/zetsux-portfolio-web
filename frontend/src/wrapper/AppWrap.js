fb159d4d2cce55aa6c095f2715e15d53c09cf9a3
zetsuen <kevnhalim@gmail.com>
2023-02-09 00:22:50 +0700

    fix : warning in web console

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -6,7 +6,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
     <div id={idName} className={`app__container ${classNames}`}>
         <SocialMedia />
 
-        <div class="app__wrapper app__flex">
+        <div className="app__wrapper app__flex">
             <Component />
 
             <div className="copyright">
