8c0df36133a8b51ff5a5617903a6eef2f551c331
zetsuen <kevnhalim@gmail.com>
2023-02-09 00:50:47 +0700

    feat : adding experience section

--- frontend/src/containers/Skills/Skills.jsx
+++ frontend/src/containers/Skills/Skills.jsx
@@ -1,27 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import './Skills.scss';
-import { ReactTooltip } from 'react-tooltip';
 import { motion } from 'framer-motion';
+import { Tooltip } from 'react-tooltip';
+
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
+import './Skills.scss';
 
 const Skills = () => {
-
   const [experiences, setExperiences] = useState([]);
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
     const query = '*[_type == "experiences"]';
-    const skillQuery = '*[_type == "skills"]';
-    client.fetch(query)
-      .then((data) => {
-        setExperiences(data);
-      })
+    const skillsQuery = '*[_type == "skills"]';
 
-    client.fetch(skillQuery)
-      .then((data) => {
-        setSkills(data);
-      })
+    client.fetch(query).then((data) => {
+      setExperiences(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
   }, []);
 
   return (
@@ -47,9 +46,46 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
+        <div className="app__skills-exp">
+          {experiences.map((experience) => (
+            <motion.div
+              className="app__skills-exp-item"
+              key={experience.year}
+            >
+              <div className="app__skills-exp-year">
+                <p className="bold-text">{experience.year}</p>
+              </div>
+              <motion.div className="app__skills-exp-works">
+                {experience.works.map((work) => (
+                  <>
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      transition={{ duration: 0.5 }}
+                      className="app__skills-exp-work"
+                      id={work.name}
+                      key={work.name}
+                    >
+                      <h4 className="bold-text">{work.name}</h4>
+                      <p className="p-text">{work.company}</p>
+                    </motion.div>
+                    
+                    <Tooltip
+                      anchorId={work.name}
+                      effect="solid"
+                      arrowColor="#fff"
+                      className="skills-tooltip"
+                    >
+                      {work.desc}
+                    </Tooltip>
+                  </>
+                ))}
+              </motion.div>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </>
   );
-}
+};
 
-export default Skills;
+export default AppWrap(Skills, 'skills');
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import './index.css';
+import 'react-tooltip/dist/react-tooltip.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
