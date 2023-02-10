391874acaa7c7b59679e71ab8c9d4db6beb49c94
zetsuen <kevnhalim@gmail.com>
2023-02-11 00:11:24 +0700

    feat : color change

--- frontend/src/components/NavigationDots.jsx
+++ frontend/src/components/NavigationDots.jsx
@@ -10,7 +10,7 @@ const NavigationDots = ({ active }) => (
         href={`#${item}`}
         key={item + index}
         className="app__navigation-dot"
-        style={active === item ? { backgroundColor: '#313BAC' } : {}}
+        style={active === item ? { backgroundColor: '#142D4C' } : {}}
       />
     ))}
   </div>
