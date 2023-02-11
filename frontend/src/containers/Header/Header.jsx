bb8f903332a246ef862d5a87036bb659541914b9
zetsuen <kevnhalim@gmail.com>
2023-02-11 11:53:05 +0700

    feat : changing icons in header

--- frontend/src/containers/Header/Header.jsx
+++ frontend/src/containers/Header/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        {[images.python, images.node, images.api].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="Circle" />
           </div>
