1c662c4f82e56c56915334838e3d7d54998ed53b
Fehlling Halim <kevnhalim@gmail.com>
2023-03-14 12:55:55 +0700

    feat : adding backend directory

---
+++ backend/schemas/index.js
@@ -0,0 +1,10 @@
+import projects from './projects'
+import testimonials from './testimonials'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+export const schemaTypes = [projects, testimonials, brands, abouts, skills, workExperience, experiences, contact]
---
+++ backend/schemas/workExperience.js
@@ -0,0 +1,21 @@
+export default {
+    name:'workExperiences',
+    title:'Work Experiences',
+    type:'document',
+    fields:[
+           {name:'name',
+               title:'name',
+               type:'string'
+            },
+            {
+                name:'company',
+                title:'Company',
+                type:'string'
+            },
+            {
+                name:'desc',
+                title:'Desc',
+                type:'string'
+            }
+    ]
+}
