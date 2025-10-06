d7ea359e04c67a749fa51840265941b4968f989b
zetsux <kevnhalim@gmail.com>
2025-10-06 19:07:29 +0700

    feat: adjust contents

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import { NavigationDots, SocialMedia } from '../components';
 
-const AppWrap = (Component, idName, classNames) => function HOC() {
-  return (
-    <div id={idName} className={`app__container ${classNames}`}>
+const AppWrap = (Component, idName, classNames) =>
+  function HOC() {
+    const currentYear = new Date().getFullYear();
+
+    return (
+      <div id={idName} className={`app__container ${classNames}`}>
         <SocialMedia />
 
-        <div className="app__wrapper app__flex">
-            <Component />
+        <div className='app__wrapper app__flex'>
+          <Component />
 
-            <div className="copyright">
-                <p className="p-text">@2023 Kevin Nathanael H.</p>
-                <p className="p-text">All rights reserved</p>
-            </div>
+          <div className='copyright'>
+            <p className='p-text'>@{currentYear} Kevin Nathanael H.</p>
+            <p className='p-text'>All rights reserved</p>
+          </div>
         </div>
 
         <NavigationDots active={idName} />
-    </div>
-  );
-}
+      </div>
+    );
+  };
 
 export default AppWrap;
